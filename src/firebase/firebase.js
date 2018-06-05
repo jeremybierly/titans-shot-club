9025563a7f339d78bed6c5441f40678e7a080aa0
Jeremy Bierly <jeremy.bierly@footlocker.com>
2018-06-04 23:24:46 -0500

    environment-variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyB3DAEEKTfxKZFWrBa0EMnIIvKdxn-9378",
-    authDomain: "shot-tracker-101f2.firebaseapp.com",
-    databaseURL: "https://shot-tracker-101f2.firebaseio.com",
-    projectId: "shot-tracker-101f2",
-    storageBucket: "shot-tracker-101f2.appspot.com",
-    messagingSenderId: "310907900161"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASER_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
 
 if (!firebase.apps.length) {
