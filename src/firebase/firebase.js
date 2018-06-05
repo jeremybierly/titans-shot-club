ca431e4093abe06220674ccaa50c24bf92460209
Jeremy Bierly <jeremy.bierly@footlocker.com>
2018-06-04 23:26:04 -0500

    env-updates

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,9 +4,9 @@ const config = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASER_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
 
 if (!firebase.apps.length) {
