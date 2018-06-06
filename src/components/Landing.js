2170c3003a911629eacf6e15954269e4a01258f5
Jeremy Bierly <jeremy.bierly@footlocker.com>
2018-06-05 20:57:40 -0500

    updates homepage

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from '../logo.svg';
 
 const LandingPage = () =>
   <div class="landing">
     <h1>10,000 Shot Club</h1>
     <p>The Glen Ellyn Titans 10,000 Shot Program is a chance for you to improve your shooting skills over the summer giving you a goal you can achieve.</p>
-    <p><img src="//cdn2.sportngin.com/attachments/text_block/9746/8812/2018_10000_Shot_Club_large.jpg" alt="2017 Members" /></p>
+    <p className="homePage"><img src={logo} className="landing-logo" alt="logo" /></p>
     <p><a href="/signin">Sign In</a> or <a href="/signup">Sign Up</a>!</p>
   </div>
 
