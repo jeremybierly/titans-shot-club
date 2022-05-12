c46b283c9c636a5f9170355e4d51ca46ed883f3f
Jeremy Bierly <91389349+jeremybierly@users.noreply.github.com>
2022-05-11 21:16:55 -0500

    Update Landing.js

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from '../logo.svg';
 
 const LandingPage = () =>
   <div class="landing">
     <h1>10,000 Shot Club and 50,000 Dribble Club</h1>
     <p>The Glen Ellyn Titans 10,000 Shot / 50,000 Dribble Program is a chance for you to improve your basketball skills over the off season giving you a goal you can achieve.</p>
-    <p className="homePage"><img src={logo} className="landing-logo" alt="logo" /></p>
+    <p className="homePage"><img src="https://cdn1.sportngin.com/attachments/banner_graphic/ddbe-165484395/SiteHeader.png" className="landing-logo" alt="logo" /></p>
     <p><a href="/signin">Sign In</a> or <a href="/signup">Sign Up</a>!</p>
     <br/><br/>
   </div>
