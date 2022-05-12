bc9b364c75b5efd997a6ee46116eb69a2c5d96d2
Jeremy Bierly <91389349+jeremybierly@users.noreply.github.com>
2022-05-11 21:10:43 -0500

    Update ShotTotals.js

--- src/components/ShotTotals.js
+++ src/components/ShotTotals.js
@@ -42,7 +42,7 @@ class ShotTotals extends Component {
     render() {
         let user = this.props.authUser ? this.props.authUser.uid : ""
         const percentage = this.state.madeShots > 0 ? Math.round(this.state.madeShots / this.state.attemptedShots * 100).toString() + "%" : "0%";
-        const then = new Date("09/03/2021");
+        const then = new Date("09/01/2022");
         const now = new Date();
         const days = Math.ceil((then-now)/(1000*60*60*24));
         const shotsLeft = 10000-this.state.campOffset-this.state.attemptedShots;
